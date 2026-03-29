a390ada3caff9030c6ad679c3f94e22af67cd5f3
aliswemohmad2 <aliswemohmad2@gmail.com>
2026-03-29 14:50:51 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -10,21 +10,7 @@ int mult(a,b){
   return a * b ;
 }
 
-int divid(a , b ){
-  return a / b ;
-}
 
-int mudolo(a , b ){
-  return a % b ;
-}
-
-int sqrt(a,b){
-  int x =a;
-  for(int = 0 ; i < b ; i++){
-    x *= a;
-  }
-  return x;
-}
 
 
 
